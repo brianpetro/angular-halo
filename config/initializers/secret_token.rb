8755bc7d1f4109c64ef96e3912dd945d19ea70c5
brianpetro <brian@plexm.com>
2013-04-11 18:56:09 -0400

    initial generation 'new myapp --edge --skip-bundle --database=postgresql --skip-test-unit --skip-index-html'

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+AngularHalo::Application.config.secret_key_base = '6619439326172bb95b70c3dcb90702644f29a3455efd5964ba32fca465e6391a640ccb311cd3c032ae002aa51e35aa1bc7df327c3ed4485d62c889c9dca14258'
