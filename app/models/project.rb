97f092e15b7389a0da5452cc09114b294ceda0da
brianpetro <brian@plexm.com>
2013-04-12 09:11:59 -0400

    make accept hstore

--- app/models/project.rb
+++ app/models/project.rb
@@ -1,2 +1,14 @@
 class Project < ActiveRecord::Base
+
+  %w[name author].each do |key|
+    scope "has_#{key}", lambda { |value| where("details @> hstore(?, ?)", key, value) }
+
+    define_method(key) do
+      details && details[key]
+    end
+
+    define_method("#{key}=") do |value|
+      self.details = (details || {}).merge(key => value)
+    end
+  end
 end
