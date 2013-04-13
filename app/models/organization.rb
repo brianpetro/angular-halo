f4054917073446a640495147bb5db1e152a1da4d
brianpetro <brian@plexm.com>
2013-04-13 12:37:13 -0400

    add model methods for hstore

--- app/models/organization.rb
+++ app/models/organization.rb
@@ -1,2 +1,13 @@
 class Organization < ActiveRecord::Base
+  %w[is_municipality].each do |key|
+    scope "has_#{key}", lambda { |value| where("data @> hstore(?, ?)", key, value) }
+
+    define_method(key) do
+      data && data[key]
+    end
+
+    define_method("#{key}=") do |value|
+      self.data = (data || {}).merge(key => value)
+    end
+  end
 end
