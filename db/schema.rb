c0c9714a024c83bb6bc0b14bbcba5db22edbe5a0
brianpetro <brian@plexm.com>
2013-04-14 14:47:57 -0400

    run rake db:migrate on new generation

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,13 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20130413162856) do
+ActiveRecord::Schema.define(version: 20130414184703) do
+
+  create_table "entries", force: true do |t|
+    t.hstore   "data"
+    t.datetime "created_at"
+    t.datetime "updated_at"
+  end
 
   create_table "organizations", force: true do |t|
     t.hstore   "data"
