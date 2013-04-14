c2c9c34116032e172c615080c527f293bd6f095d
brianpetro <brian@plexm.com>
2013-04-14 14:53:15 -0400

    make hstore work throughn model

--- app/models/entry.rb
+++ app/models/entry.rb
@@ -1,2 +1,13 @@
 class Entry < ActiveRecord::Base
+  %w[title summary].each do |key|
+    scope "has_#{key}", lambda { |value| where("data @> hstore(?, ?)", key, value) }
+
+    define_method(key) do
+      data && data[key]
+    end
+
+    define_method("#{key}=") do |value|
+      self.data = (data || {}).merge(key => value)
+    end
+  end
 end
