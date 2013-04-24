7ff2813216387cbc6324770ff053307071e67124
brianpetro <brian@plexm.com>
2013-04-24 17:24:32 -0400

    modify feature name and generate project data feature

--- test/features/can_access_home_test.rb
+++ test/features/can_access_home_test.rb
@@ -1,7 +1,7 @@
 require "minitest_helper"
 
 # To be handled correctly by Capybara this spec must end with "Feature Test"
-feature "CanAccessHome Feature Test" do
+feature "Can Access Home Feature Test" do
   scenario "Contains text 'Listing Projects'" do
     visit root_path
     page.must_have_content "Listing projects"
---
+++ test/features/project_page_contains_project_data_test.rb
@@ -0,0 +1,10 @@
+require "minitest_helper"
+
+# To be handled correctly by Capybara this spec must end with "Feature Test"
+feature "ProjectPageContainsProjectData Feature Test" do
+  scenario "the test is sound" do
+    visit root_path
+    page.must_have_content "Hello World"
+    page.wont_have_content "Goobye All!"
+  end
+end
