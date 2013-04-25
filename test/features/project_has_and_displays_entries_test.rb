b67d6f3cd6461c45f6be3fc8347587ef6d52d939
brianpetro <brian@plexm.com>
2013-04-25 18:41:43 -0400

    display entry id and add test. Entry test failing since change to angular js.

--- test/features/project_has_and_displays_entries_test.rb
+++ test/features/project_has_and_displays_entries_test.rb
@@ -2,6 +2,15 @@ require "minitest_helper"
 
 # To be handled correctly by Capybara this spec must end with "Feature Test"
 feature "Project Show Has And Displays Entries Feature Test" do
+  scenario "Displays Entries ID" do
+  	project = Project.create!(details: {name: "aproject"})
+    e1 = Entry.create!(data: {}, project_id: project.id)
+  	e2 = Entry.create!(data: {})
+    visit project_path(project.id)
+    page.must_have_content e1.id
+    page.wont_have_content e2.id
+  end
+
   scenario "Displays Entries Title" do
   	project = Project.create!(details: {name: "aproject"})
   	Entry.create!(data: {title: "Entry Title Belongs"}, project_id: project.id)
@@ -13,8 +22,8 @@ feature "Project Show Has And Displays Entries Feature Test" do
 
   scenario "Displays Entries Summary" do
   	project = Project.create!(details: {name: "aproject"})
-    Entry.create!(data: {summary: "Should Be Displayed"}, project_id: project.id)
-  	Entry.create!(data: {summary: "Should Not Be Displayed"})
+    e1 = Entry.create!(data: {summary: "Should Be Displayed"}, project_id: project.id)
+  	e2 = Entry.create!(data: {summary: "Should Not Be Displayed"})
     visit project_path(project.id)
     page.must_have_content "Should Be Displayed"
     page.wont_have_content "Should Not Be Displayed"
