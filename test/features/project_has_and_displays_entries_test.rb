6062630df3391ba39d6c96aa4161b9a548a92b49
brianpetro <brian@plexm.com>
2013-04-25 11:21:31 -0400

    testing entries on project show page passing.

--- test/features/project_has_and_displays_entries_test.rb
+++ test/features/project_has_and_displays_entries_test.rb
@@ -2,14 +2,21 @@ require "minitest_helper"
 
 # To be handled correctly by Capybara this spec must end with "Feature Test"
 feature "Project Show Has And Displays Entries Feature Test" do
-  scenario "Project Shows Has and Displays Projects Entries" do
+  scenario "Displays Entries Title" do
   	project = Project.create!(details: {name: "aproject"})
-  	Entry.create!(data: {title: "Entry Title Belongs", summary: "Should Be Displayed"}, project_id: project.id)
-  	Entry.create!(data: {title: "Do Not Display", summary: "Should Not Be Displayed"})
+  	Entry.create!(data: {title: "Entry Title Belongs"}, project_id: project.id)
+  	Entry.create!(data: {title: "Do Not Display"})
     visit project_path(project.id)
     page.must_have_content "Entry Title Belongs"
-    page.must_have_content "Should Be Displayed"
     page.wont_have_content "Do Not Display"
+  end
+
+  scenario "Displays Entries Summary" do
+  	project = Project.create!(details: {name: "aproject"})
+    Entry.create!(data: {summary: "Should Be Displayed"}, project_id: project.id)
+  	Entry.create!(data: {summary: "Should Not Be Displayed"})
+    visit project_path(project.id)
+    page.must_have_content "Should Be Displayed"
     page.wont_have_content "Should Not Be Displayed"
   end
 end
