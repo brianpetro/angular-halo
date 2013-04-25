85b1c8dbc6d664b79181adbc95f121fea3b2a485
brianpetro <brian@plexm.com>
2013-04-24 20:14:09 -0400

    test project show page for name deatil

--- test/features/project_page_contains_project_data_test.rb
+++ test/features/project_page_contains_project_data_test.rb
@@ -1,10 +1,10 @@
 require "minitest_helper"
 
 # To be handled correctly by Capybara this spec must end with "Feature Test"
-feature "ProjectPageContainsProjectData Feature Test" do
-  scenario "the test is sound" do
-    visit root_path
-    page.must_have_content "Hello World"
-    page.wont_have_content "Goobye All!"
+feature "Project Show Page Contains Project Details Feature Test" do
+  scenario "Displays Project Name" do
+  	project = Project.create!(details: {name: "aproject"})
+    visit project_path(project.id)
+    page.must_have_content project.name
   end
 end
