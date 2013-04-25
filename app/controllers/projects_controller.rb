de9fa513ca7e3d1d9165a9dc2c2bca214f29dc8f
brianpetro <brian@plexm.com>
2013-04-25 11:18:50 -0400

    add global entries to private before action

--- app/controllers/projects_controller.rb
+++ app/controllers/projects_controller.rb
@@ -1,5 +1,6 @@
 class ProjectsController < ApplicationController
   before_action :set_project, only: [:show, :edit, :update, :destroy]
+  before_action :get_entries, only: [:show]
 	respond_to :json, :html
   # GET /projects
   # GET /projects.json
@@ -73,6 +74,10 @@ class ProjectsController < ApplicationController
     # Use callbacks to share common setup or constraints between actions.
     def set_project
       @project = Project.find(params[:id])
+    end
+
+    def get_entries
+			@entries = Entry.where(project_id: @project.id)
     end
 
     # Never trust parameters from the scary internet, only allow the white list through.
