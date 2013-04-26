d789c323f62887a8e6ce78e35823f286f95ec5c8
brianpetro <brian@plexm.com>
2013-04-26 19:31:35 -0400

    make organization_id attr accessible

--- app/controllers/projects_controller.rb
+++ app/controllers/projects_controller.rb
@@ -82,6 +82,6 @@ class ProjectsController < ApplicationController
 
     # Never trust parameters from the scary internet, only allow the white list through.
     def project_params
-      params.require(:project).permit(:name, :author)
+      params.require(:project).permit(:name, :author, :organization_id)
     end
 end
