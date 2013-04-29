02fa5341640863b887c7d86135f34905d0178d4b
brianpetro <brian@plexm.com>
2013-04-29 18:23:10 -0400

    set js as true, tests PASS

--- test/features/project_has_and_displays_entries_test.rb
+++ test/features/project_has_and_displays_entries_test.rb
@@ -6,19 +6,19 @@ feature "Project Show Has And Displays Entries Feature Test" do
   e1 = Entry.create!(data: {title: "Entry Title Belongs", summary: "Should Be Displayed"}, project_id: project.id)
   e2 = Entry.create!(data: {title: "Do Not Display", summary: "Should Not Be Displayed"})
 
-  scenario "Displays Entries ID" do
+  scenario "Displays Entries ID", js: true do
     visit project_path(project.id)
-    page.must_have_content e1.id
-    page.wont_have_content e2.id
+    page.must_have_content e1.id.to_s
+    page.wont_have_content e2.id.to_s
   end
 
-  scenario "Displays Entries Title" do
+  scenario "Displays Entries Title", js: true do
     visit project_path(project.id)
     page.must_have_content "Entry Title Belongs"
     page.wont_have_content "Do Not Display"
   end
 
-  scenario "Displays Entries Summary" do
+  scenario "Displays Entries Summary", js: true do
     visit project_path(project.id)
     page.must_have_content "Should Be Displayed"
     page.wont_have_content "Should Not Be Displayed"
