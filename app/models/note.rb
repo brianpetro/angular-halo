8f857b837a5fc235c65b6cad20b1788140d5510d
brianpetro <brian@plexm.com>
2013-04-29 16:34:11 -0400

    setup model for hstore data

--- app/models/note.rb
+++ app/models/note.rb
@@ -1,2 +1,13 @@
 class Note < ActiveRecord::Base
+  %w[tldr concern author].each do |key|
+    scope "has_#{key}", lambda { |value| where("data @> hstore(?, ?)", key, value) }
+
+    define_method(key) do
+      data && data[key]
+    end
+
+    define_method("#{key}=") do |value|
+      self.data = (data || {}).merge(key => value)
+    end
+  end
 end
