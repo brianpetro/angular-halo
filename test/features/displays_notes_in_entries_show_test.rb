dcb026f895a4a5045a1979fb567d319e12c49c82
brianpetro <brian@plexm.com>
2013-04-29 19:36:30 -0400

    test note displays on entry show page, PASSES

--- test/features/displays_notes_in_entries_show_test.rb
+++ test/features/displays_notes_in_entries_show_test.rb
@@ -2,12 +2,25 @@ require "minitest_helper"
 
 # To be handled correctly by Capybara this spec must end with "Feature Test"
 feature "Displays Notes In Entries Show Feature Test" do
-  scenario "tldr is displayed" do
-  	entry = Entry.create!(data: {title: "entry1"})
-    n1 = Note.create!(data: {tldr: "display"}, entry_id: entry.id)
-  	n2 = Note.create!(data: {tldr: "dont display"})
+	entry = Entry.create!(data: {title: "entry1"})
+  n1 = Note.create!(tldr: "display", concern: "display_concern", author: "display_brian", entry_id: entry.id)
+	n2 = Note.create!(tldr: "dont_display", concern: "no_concern", author: "no_brian")
+
+  scenario "tldr is displayed", js: true do
     visit entry_path(entry.id)
-    page.must_have_content "display"
-    page.wont_have_content "dont display"
+    page.must_have_content n1.tldr
+    page.wont_have_content n2.tldr
+  end
+
+  scenario "concern is displayed", js: true do
+    visit entry_path(entry.id)
+    page.must_have_content n1.concern
+    page.wont_have_content n2.concern
+  end
+
+  scenario "author is displayed", js: true do
+    visit entry_path(entry.id)
+    page.must_have_content n1.author
+    page.wont_have_content n2.author
   end
 end
